538b8401c3bb474b1d0e652df10d75852b64f91a
Casey Deccio <casey@deccio.net>
2025-02-06 14:17:50 -0700

    Update mostly comments from client.c and server.c

--- 07-hw-sockets/client.c
+++ 07-hw-sockets/client.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char *argv[]) {
 
-	/* Check usage */
+	// Check usage
 	if (argc < 3 ||
 		((strcmp(argv[1], "-4") == 0 || strcmp(argv[1], "-6") == 0) &&
 			argc < 4)) {
@@ -21,9 +21,9 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	/* Use only IPv4 (AF_INET) if -4 is specified;
-	 * Use only IPv6 (AF_INET6) if -6 is specified;
-	 * Try both if neither is specified. */
+	// Use only IPv4 (AF_INET) if -4 is specified;
+	// Use only IPv6 (AF_INET6) if -6 is specified;
+	// Try both if neither is specified.
 	int af = AF_UNSPEC;
 	int hostindex;
 	if (strcmp(argv[1], "-4") == 0 ||
@@ -38,18 +38,22 @@ int main(int argc, char *argv[]) {
 		hostindex = 1;
 	}
 
+
+	/* SECTION A - pre-socket setup; getaddrinfo() */
+
+	// Declare and initialize hints, which is passed to getaddrinfo() to
+	// get our address info.
 	struct addrinfo hints;
 	// Initialize everything to 0
 	memset(&hints, 0, sizeof(struct addrinfo));
-	// Set the address family to either AF_INET (IPv4-only), AF_INET6
-	// (IPv6-only), or AF_UNSPEC (either IPv4 or IPv6), depending on what
-	// was passed in on the command line.
+	// Set the address family to either AF_INET (get only IPv4 address
+	// information), AF_INET6 (get only IPv6 information), or AF_UNSPEC
+	// (get both IPv4 and IPv6 information), depending on what was passed
+	// in on the command line.
 	hints.ai_family = af;
 	// Use type SOCK_DGRAM (UDP)
 	hints.ai_socktype = SOCK_DGRAM;
 
-
-	/* SECTION A - pre-socket setup; getaddrinfo() */
 
 	struct addrinfo *result;
 	int s;
@@ -61,23 +65,25 @@ int main(int argc, char *argv[]) {
 	}
 
 
-	/* SECTION B - pre-socket setup; getaddrinfo() */
+	/* SECTION B - set up socket using results from getaddrinfo */
 
 	int sfd;
 	int addr_fam;
 	socklen_t addr_len;
 
-	// Address information is stored in local_addr_ss, which is of type
-	// struct addr_storage.  However, all functions require a parameter of
-	// type struct sockaddr *.  Instead of type-casting everywhere, we
-	// declare local_addr, which is of type struct sockaddr *, point it to
-	// the address of local_addr_ss, and use local_addr everywhere.
+	// Local address information is stored in local_addr_ss, which is of
+	// type struct addr_storage.  However, all functions require a
+	// parameter of type struct sockaddr *.  Instead of type-casting
+	// everywhere, we declare local_addr, which is of type
+	// struct sockaddr *, point it to the address of local_addr_ss, and use
+	// local_addr everywhere.
 	struct sockaddr_storage local_addr_ss;
 	struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
 	char local_ip[INET6_ADDRSTRLEN];
 	unsigned short local_port;
 
-	// See notes above for local_addr_ss and local_addr_ss.
+	// Remote address information is stored in remote_addr_ss.  See notes
+	// above for local_addr_ss and local_addr_ss.
 	struct sockaddr_storage remote_addr_ss;
 	struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
 	char remote_ip[INET6_ADDRSTRLEN];
@@ -86,7 +92,7 @@ int main(int argc, char *argv[]) {
 	struct addrinfo *rp;
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
 		// Loop through every entry in the linked list populated by
-		// getaddrinfo().   If socket(2) (or connect(2)) fails, we
+		// getaddrinfo().   If either socket(2) or connect(2) fails, we
 		// close the socket and try the next address.
 		//
 		// For each iteration of the loop, rp points to an instance of
@@ -113,9 +119,11 @@ int main(int argc, char *argv[]) {
 		// pointed to by remote_addr.
 		memcpy(remote_addr, rp->ai_addr, sizeof(struct sockaddr_storage));
 
-		// Extract the remote IP address and port from remote_addr
-		// using parse_sockaddr().  parse_sockaddr() is defined in
-		// ../code/sockhelper.c.
+		// Extract the IP address (as a string) and port (as an int)
+		// from the struct referred to by remote_addr, and store them
+		// in the locations referred to by remote_ip and &remote_port.
+		// This is a little messy, so we use the helper function
+		// parse_sockaddr(), which is defined in ../code/sockhelper.c.
 		parse_sockaddr(remote_addr, remote_ip, &remote_port);
 		fprintf(stderr, "Connecting to %s:%d (addr family: %d)\n",
 				remote_ip, remote_port, addr_fam);
@@ -128,24 +136,31 @@ int main(int argc, char *argv[]) {
 		close(sfd);
 	}
 
-	// connect() did not succeed with any address returned by
-	// getaddrinfo().
 	if (rp == NULL) {
+		// If rp is NULL, then connect() did not succeed with any
+		// address returned by getaddrinfo().
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
 	}
 
-	// Free the member associated with the linked list populated by
+	// Free the memory associated with the linked list populated by
 	// getaddrinfo()
 	freeaddrinfo(result);
 
-	// NOTE: addrlen needs to be initialized before every call to
+	// Extract the local IP address and port associated with sfd into the
+	// struct sockaddr_storage referenced by local_addr.  This is done
+	// using the system call getsockname().
+	//
+	// NOTE: addr_len needs to be initialized before every call to
 	// getsockname().  See the man page for getsockname().
 	addr_len = sizeof(struct sockaddr_storage);
 	s = getsockname(sfd, local_addr, &addr_len);
-	// Extract the IP address and port from remote_addr using
-	// parse_sockaddr().  parse_sockaddr() is defined in
-	// ../code/sockhelper.c.
+
+	// Extract the IP address (as a string) and port (as an int)
+	// from the struct referred to by local_addr, and store them
+	// in the locations referred to by local_ip and &local_port.
+	// This is a little messy, so we use the helper function
+	// parse_sockaddr(), which is defined in ../code/sockhelper.c.
 	parse_sockaddr(local_addr, local_ip, &local_port);
 	fprintf(stderr, "Local socket info: %s:%d (addr family: %d)\n",
 			local_ip, local_port, addr_fam);
@@ -159,16 +174,16 @@ int main(int argc, char *argv[]) {
 		// buf will hold the bytes we read from the socket.
 		char buf[BUF_SIZE];
 
-		// len includes the count of all characters comprising the
-		// null-terminated string argv[j], but not the null byte
-		// itself.
 		size_t len = strlen(argv[j]);
+
+		// Check for buffer overflow.
 		if (len > BUF_SIZE) {
 			fprintf(stderr,
 					"Ignoring long message in argument %d\n", j);
 			continue;
 		}
 
+		// Send bytes to remote socket using send().
 		ssize_t nwritten = send(sfd, argv[j], len, 0);
 		if (nwritten < 0) {
 			perror("send");
@@ -176,13 +191,13 @@ int main(int argc, char *argv[]) {
 		}
 		printf("Sent %zd bytes: %s\n", len, argv[j]);
 
+		// Read bytes from remote socket using recv().
 		ssize_t nread = recv(sfd, buf, BUF_SIZE, 0);
 		buf[nread] = '\0';
 		if (nread < 0) {
 			perror("read");
 			exit(EXIT_FAILURE);
 		}
-
 		printf("Received %zd bytes: %s\n", nread, buf);
 
 	}
--- 07-hw-sockets/server.c
+++ 07-hw-sockets/server.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char *argv[]) {
 
-	/* Check usage */
+	// Check usage
 	if (!(argc == 2 || (argc == 3 &&
 			(strcmp(argv[1], "-4") == 0 || strcmp(argv[1], "-6") == 0)))) {
 		fprintf(stderr, "Usage: %s [ -4 | -6 ] port\n", argv[0]);
@@ -27,8 +27,8 @@ int main(int argc, char *argv[]) {
 		portindex = 2;
 	}
 
-	/* Use IPv4 by default (or if -4 is specified);
-	 * If -6 is specified, then use IPv6 instead. */
+	// Use IPv4 by default (or if -4 is specified);
+	// If -6 is specified, then use IPv6 instead.
 	int addr_fam;
 	if (argc == 2 || strcmp(argv[1], "-4") == 0) {
 		addr_fam = AF_INET;
@@ -39,28 +39,32 @@ int main(int argc, char *argv[]) {
 	unsigned short port = atoi(argv[portindex]);
 	int sock_type = SOCK_DGRAM;
 
+
 	/* SECTION A - populate address structures */
 
-	/* SECTION B - setup socket */
+	// Local address information is stored in local_addr_ss, which is of
+	// type struct addr_storage.  However, all functions require a
+	// parameter of type struct sockaddr *.  Instead of type-casting
+	// everywhere, we declare local_addr, which is of type
+	// struct sockaddr *, point it to the address of local_addr_ss, and use
+	// local_addr everywhere.
+	struct sockaddr_storage local_addr_ss;
+	struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
+
+	// Populate local_addr with the specified port and with default
+	// address (indicated with NULL).  This is a little messy, so we use
+	// the helper function parse_sockaddr(), which is defined in
+	// ../code/sockhelper.c.
+	populate_sockaddr(local_addr, addr_fam, NULL, port);
+
+
+	/* SECTION B - set up socket */
 
 	int sfd;
 	if ((sfd = socket(addr_fam, sock_type, 0)) < 0) {
 		perror("Error creating socket");
 		exit(EXIT_FAILURE);
 	}
-
-	// Declare structures for local address and port.
-	//
-	// Address information is stored in local_addr_ss, which is of type
-	// struct addr_storage.  However, all functions require a parameter of
-	// type struct sockaddr *.  Instead of type-casting everywhere, we
-	// declare local_addr, which is of type struct sockaddr *, point it to
-	// the address of local_addr_ss, and use local_addr everywhere.
-	struct sockaddr_storage local_addr_ss;
-	struct sockaddr *local_addr = (struct sockaddr *)&local_addr_ss;
-
-	// Populate local_addr with the port using populate_sockaddr().
-	populate_sockaddr(local_addr, addr_fam, NULL, port);
 	if (bind(sfd, local_addr, sizeof(struct sockaddr_storage)) < 0) {
 		perror("Could not bind");
 		exit(EXIT_FAILURE);
@@ -73,14 +77,19 @@ int main(int argc, char *argv[]) {
 	while (1) {
 		char buf[BUF_SIZE];
 
-		// Declare structures for remote address and port.
-		// See notes above for local_addr_ss and local_addr_ss.
+		// Remote address information is stored in remote_addr_ss.  See notes
+		// above for local_addr_ss and local_addr_ss.
 		struct sockaddr_storage remote_addr_ss;
 		struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
 		char remote_ip[INET6_ADDRSTRLEN];
 		unsigned short remote_port;
 
-		// NOTE: addrlen needs to be initialized before every call to
+		// Read bytes from remote socket using recvfrom().  The remote
+		// IP address and port from which the message was sent is
+		// stored in the struct sockaddr_storage referenced by
+		// remote_addr.
+		//
+		// NOTE: addr_len needs to be initialized before every call to
 		// recvfrom().  See the man page for recvfrom().
 		socklen_t addr_len = sizeof(struct sockaddr_storage);
 		ssize_t nread = recvfrom(sfd, buf, BUF_SIZE, 0,
@@ -90,13 +99,16 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 		}
 
-		// Extract the IP address and port from remote_addr using
-		// parse_sockaddr().  parse_sockaddr() is defined in
-		// ../code/sockhelper.c.
+		// Extract the IP address and port from the struct referred to
+		// by remote_addr using parse_sockaddr(), and store them in the
+		// locations referred to by remote_ip and &remote_port.  This
+		// is a little messy, so we use the helper function
+		// parse_sockaddr(), which is defined in ../code/sockhelper.c.
 		parse_sockaddr(remote_addr, remote_ip, &remote_port);
 		printf("Received %zd bytes from %s:%d\n",
 				nread, remote_ip, remote_port);
 
+		// Return bytes to remote socket using sendto().
 		if (sendto(sfd, buf, nread, 0, remote_addr, addr_len) < 0) {
 			perror("sending response");
 		}
